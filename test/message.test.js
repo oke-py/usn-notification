17f40a9913e8de205bce04665749cd9ecbc4fa21
Naoki Oketani <okepy.naoki@gmail.com>
2019-06-12 12:31:59 +0900

    add test case: increase branch coverage (#5)

--- test/message.test.js
+++ test/message.test.js
@@ -6,7 +6,7 @@ const message = require('../src/message');
 
 describe('message', () => {
   describe('#create', () => {
-    it('should create slack attachment', () => {
+    it('should create slack attachment for affected version', () => {
       const stream = {
         "ApproximateCreationDateTime": 1560307609,
         "Keys": {
@@ -81,6 +81,91 @@ describe('message', () => {
               {
                 "title": "Ubuntu 18.04 LTS",
                 "value": "Affected",
+                "short": true
+              }
+            ]
+          }
+        ]
+      };
+
+      assert.deepEqual(message.create(stream.NewImage), expected);
+    });
+
+    it('should create slack attachment for non-affected version', () => {
+      const stream = {
+        "ApproximateCreationDateTime": 1560307609,
+        "Keys": {
+          "usn_id": {
+            "S": "USN-4016-1"
+          },
+          "published": {
+            "S": "2019-06-11T17:34:10Z"
+          }
+        },
+        "NewImage": {
+          "severity": {
+            "S": "Medium"
+          },
+          "affects_1604": {
+            "BOOL": false
+          },
+          "affects_1804": {
+            "BOOL": false
+          },
+          "usn_id": {
+            "S": "USN-4016-1"
+          },
+          "name": {
+            "S": "vim"
+          },
+          "published": {
+            "S": "2019-06-11T17:34:10Z"
+          },
+          "updated": {
+            "S": "2019-06-11T17:34:10Z"
+          },
+          "CVEs": {
+            "L": [
+              {
+                "S": "CVE-2017-5953"
+              },
+              {
+                "S": "CVE-2019-12735"
+              }
+            ]
+          }
+        },
+        "SequenceNumber": "22285000000000022260606301",
+        "SizeBytes": 200,
+        "StreamViewType": "NEW_AND_OLD_IMAGES"
+      }
+
+      const expected = {
+        "attachments": [
+          {
+            "fallback": "USN-4016-1: vim vulnerability",
+            "color": "danger",
+            "title": "USN-4016-1",
+            "title_link": "https://usn.ubuntu.com/4016-1/",
+            "fields": [
+              {
+                "title": "Package name",
+                "value": "vim",
+                "short": true
+              },
+              {
+                "title": "Priority",
+                "value": "Medium",
+                "short": true
+              },
+              {
+                "title": "Ubuntu 16.04 LTS",
+                "value": "Not Affected",
+                "short": true
+              },
+              {
+                "title": "Ubuntu 18.04 LTS",
+                "value": "Not Affected",
                 "short": true
               }
             ]
